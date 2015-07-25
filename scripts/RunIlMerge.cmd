44b2105a8daac01c37d9029e2fe567ba661d2699
Oren Novotny <oren@novotny.org>
2015-07-25 12:27:39 -0400

    add cmd to invoke ilmerge

---
+++ scripts/RunIlMerge.cmd
@@ -0,0 +1,4 @@
+if not exist "%~dp0..\artifacts\tools\dotnet" mkdir "%~dp0..\artifacts\tools\dotnet"
+
+"%~dp0..\packages\ilmerge.2.14.1208\tools\ilmerge.exe" "%~dp0..\src\ReferenceGenerator\bin\%1\ReferenceGenerator.exe" "%~dp0..\src\ReferenceGenerator\bin\%1\System.Collections.Immutable.dll" "%~dp0..\src\ReferenceGenerator\bin\%1\System.Reflection.Metadata.dll" /out:"%~dp0..\artifacts\tools\dotnet\RefGen.exe"  /internalize   /targetplatform:"v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5" 
+copy "%~dp0..\src\ReferenceGenerator\bin\%1\ReferenceGenerator.exe.config" "%~dp0..\artifacts\tools\dotnet\RefGen.exe.config" /y
