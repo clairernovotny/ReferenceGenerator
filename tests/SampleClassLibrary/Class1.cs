68dd75b77c6380de3bc18c849d37b438bedf60f8
Oren Novotny <oren@novotny.org>
2015-07-25 13:26:07 -0400

    Add test assms

---
+++ tests/SampleClassLibrary/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Xml.Linq;
+
+namespace SampleClassLibrary
+{
+    public class Class1
+    {
+        XDocument doc;
+        public Class1()
+        {
+            var md = this.GetType().GetMethod("foo");
+        }
+    }
+}
---
+++ tests/SampleCoreLibrary/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Xml.Linq;
+
+namespace SampleCoreLibrary
+{
+    public class Class1
+    {
+        XDocument doc;
+        public Class1()
+        {
+            var md = this.GetType().GetRuntimeMethod("foo", null);
+
+            var h = WebRequest.CreateHttp("http://foo.bar");
+        }
+    }
+}
