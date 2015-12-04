06682e1e8f134fc1d2a7a56b5385aa4f2e0ab295
Oren Novotny <oren@novotny.org>
2015-12-04 10:47:06 -0500

    Add PCL proj with project.json

---
+++ tests/SampleProjJsonPcl/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace SampleProjJsonPcl
+{
+    public class Class1
+    {
+        private HttpWebRequest req = WebRequest.CreateHttp("foo");
+    }
+}
