0be42dd6634c38239d7bffbb03ab299ec31400b5
Oren Novotny <oren@novotny.org>
2016-06-06 21:00:37 -0400

    Add test cross compiled project

---
+++ tests/SampleCrossCompiledLibrary/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+#if NETSTANDARD1_5
+using System.Net.Sockets;
+#endif
+using System.Threading.Tasks;
+
+namespace SampleCrossCompiledLibrary
+{
+
+    public class Class1
+    {
+#if NETSTANDARD1_5
+        TcpClient socket = new TcpClient();
+#endif
+
+
+        public Class1()
+        {
+
+        }
+    }
+}
